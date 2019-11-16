6686ee99f5a6db92c46d0971ce627e99542de124
grzegorz.klarecki <grzegorz.klarecki@gmail.com>
2019-11-16 01:44:39 +0100

    deded

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -17,7 +17,9 @@ export class ApiService {
   public httpOptions = {
     headers: new HttpHeaders({
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, Content-Type, Accept"
     })
   };
 
